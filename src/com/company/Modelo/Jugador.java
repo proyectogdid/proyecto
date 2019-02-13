378b59e868b6d496d3360a7cd8f6b746a16c5e8c
javier <javiermdg998@gmail.com>
2019-02-13 11:31:44 +0100

    arreglo git

--- src/com/company/Modelo/Jugador.java
+++ src/com/company/Modelo/Jugador.java
@@ -1,7 +1,13 @@
 package com.company.Modelo;
 
+import java.util.Date;
+
 public class Jugador {
     private int id;
-    
-
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private Date fechaNac;
+    private String dorsal;
+    private String textoCamiseta;
 }
