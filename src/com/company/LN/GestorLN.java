98adb17f078560991477ec9a10cf63f98aa640b0
javier <javiermdg998@gmail.com>
2019-02-21 12:07:58 +0100

    gestor ln prueba campos

--- src/com/company/LN/GestorLN.java
+++ src/com/company/LN/GestorLN.java
@@ -1,5 +1,15 @@
 package com.company.LN;
 
-public class GestorLN {
+import java.util.ArrayList;
 
+public class GestorLN {
+    ArrayList<Campo> campos=new ArrayList<>();
+
+    public void anadirCampo(int id, String nombre, String ciudad, String calle, String numero, String cp, int aforo){
+        Campo campo=new Campo( id,nombre,ciudad,calle,numero,cp,aforo);
+        campos.add(campo);
+    }
+    public ArrayList<Campo>leerCampos(){
+        return campos;
+    }
 }
