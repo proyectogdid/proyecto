0cf71646dcd7bf9528cd7cebfeee455bf9f6baf9
javier <javiermdg998@gmail.com>
2019-03-13 18:38:30 +0100

    crear clsConstantes

---
+++ src/com/company/comun/clsConstantes.java
@@ -0,0 +1,67 @@
+package com.company.comun;
+
+public class clsConstantes {
+
+    public static final String AFICIONADO_FAVORITO = "favorito";
+
+
+    public static final String CAMPO_ID = "id";
+    public static final String CAMPO_NOMBRE = "nombre";
+    public static final String CAMPO_CIUDAD = "ciudad";
+    public static final String CAMPO_CALLE = "calle";
+    public static final String CAMPO_NUMERO = "numero";
+    public static final String CAMPO_CP = "cp";
+    public static final String CAMPO_AFORO = "aforo";
+
+    public static final String EQUIPO_ID ="id";
+    public static final String EQUIPO_NOMBRE ="nombre";
+    public static final String EQUIPO_PATROCINADOR ="patrocinador";
+    public static final String EQUIPO_JUGADORES ="jugadores";
+
+    public static final String ESTADO_ID="id";
+    public static final String ESTADO_NOMBRE="nombre";
+
+    public static final String EVENTO_ID="id";
+    public static final String EVENTO_ESTADO_PREVIO="estadoPrevio";
+    public static final String EVENTO_ESTADO_POSTERIOR="estadoPosterior";
+    public static final String EVENTO_JUGADOR="jugador";
+
+    public static final String JUGADOR_ID="id";
+    public static final String JUGADOR_NOMBRE="nombre";
+    public static final String JUGADOR_APELLIDO1="apellido1";
+    public static final String JUGADOR_APELLIDO2="apellido2";
+    public static final String JUGADOR_FECHA_NACIMIENTO="fechaNac";
+    public static final String JUGADOR_DORSAL="dorsal";
+    public static final String JUGADOR_TEXTO_CAMISETA="textoCamiseta";
+    public static final String JUGADOR_EQUIPO="equipo";
+    public static final String JUGADOR_POSICION="posicion";
+    public static final String JUGADOR_ESTADO= "estado";
+
+    public static final String PARTIDO_ID="id";
+    public static final String PARTIDO_FECHA="fecha";
+    public static final String PARTIDO_PTOS_LOCAL= "ptosLocal";
+    public static final String PARTIDO_PTOS_VISITANTE= "ptosVisitante";
+    public static final String PARTIDO_LOCAL= "local";
+    public static final String PARTIDO_VISITANTE= "visitante";
+    public static final String PARTIDO_TEMPORADA="temporada";
+    public static final String PARTIDO_CAMPO= "campo";
+
+    public static final String POSICION_ID="id";
+    public static final String POSICION_NOMBRE="nombre";
+
+    public static final String TEMPORADA_ID="id";
+    public static final String TEMPORADA_ANO="ano";
+    public static final String TEMPORADA_PARTIDOS="partidos";
+
+    public static final String TRASPASO_ID="id";
+    public static final String TRASPASO_PRECIO="precio";
+    public static final String TRASPASO_JUGADOR= "jugador";
+    public static final String TRASPASO_EQUIPO_PREVIO= "equipoPrevio";
+    public static final String TRASPASO_EQUIPO_POSTERIOR= "equipoPosterior";
+
+    public static final String USUARIO_ID="id";
+    public static final String USUARIO_USERNAME="username";
+    public static final String USUARIO_CONTRASENA="contrasena";
+    public static final String USUARIO_TIPO="tipo";
+
+}
