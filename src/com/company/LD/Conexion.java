72690ff381c2af20118fe6d6781d2a64838f3c95
Mikel Lerena <mikellerena@opendeusto.es>
2019-04-15 16:36:32 +0200

    Actualizacion BD

--- src/com/company/LD/Conexion.java
+++ src/com/company/LD/Conexion.java
@@ -25,7 +25,7 @@ public abstract class Conexion {
 
     public static ResultSet select(Connection con, String query) throws Exception {
         Statement stt = con.createStatement();
-      //  System.out.println(query);
+        //  System.out.println(query);
         ResultSet rs = stt.executeQuery(query);
 
         return rs;
@@ -34,8 +34,8 @@ public abstract class Conexion {
     /**
      * metodo generico para actualizar registros en BD
      *
-     * @param con con
-     * @param query query
+     * @param con        con
+     * @param query      query
      * @param parametros parametros
      * @return numero lineas afectadas
      * @throws Exception fallos en la query
@@ -50,7 +50,7 @@ public abstract class Conexion {
     /**
      * metodo para hacer queries con parametros
      *
-     * @param query query
+     * @param query      query
      * @param parametros parametros
      * @return return
      * @throws Exception fallos en la query
@@ -66,59 +66,60 @@ public abstract class Conexion {
     /**
      * Metodo generico para llamar a inserts ahorrando el codigo repetido
      *
-     * @param con con
-     * @param query sentencia sql a ejecutar
+     * @param con        con
+     * @param query      sentencia sql a ejecutar
      * @param parametros parametros
      * @return id del objeto insertado
      * @throws Exception throws Exception
      */
     public static int insert(Connection con, String query, Object[] parametros) throws Exception {
-        PreparedStatement stt = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement stt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
         cargarDatos(stt, parametros);
         stt.execute();
 
-        ResultSet rs=stt.getGeneratedKeys();
-        return (rs.next())?rs.getInt(1):-1;
+        ResultSet rs = stt.getGeneratedKeys();
+        return (rs.next()) ? rs.getInt(1) : -1;
     }
 
     /**
      * metodo para hacer una insert de multiples filas
-     * @param con connexion de BD
-     * @param query insert "insert {template} into values"
+     *
+     * @param con    connexion de BD
+     * @param query  insert "insert {template} into values"
      * @param params matriz de parametros
      * @return Array de PK
      * @throws Exception Sql exception
      */
 
-    public static ArrayList<Integer> multiInsert(Connection con, String query, Object[][]params) throws Exception{
+    public static ArrayList<Integer> multiInsert(Connection con, String query, Object[][] params) throws Exception {
 
-        int filas=params.length;
-        int columnas=params[0].length;
-        for (int j = 0; j <filas ; j++) {
+        int filas = params.length;
+        int columnas = params[0].length;
+        for (int j = 0; j < filas; j++) {
 
 
             String txt = "(";
             for (int i = 0; i < columnas; i++) {
-                txt+=(i<columnas-1)?"?,":"?";
-                            }
-            txt += (j<filas-1)?"),":")";
+                txt += (i < columnas - 1) ? "?," : "?";
+            }
+            txt += (j < filas - 1) ? ")," : ")";
 
-            query+=txt;
+            query += txt;
         }
 
 
-//        System.out.println(query);
+        System.out.println(query);
 
 
-        PreparedStatement stt = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
-        cargarMultiDatos(stt,params);
+        PreparedStatement stt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        cargarMultiDatos(stt, params);
         //query builder
         //for
         //cargarDatos(stt, params);
         stt.execute();
-        ResultSet rs=stt.getGeneratedKeys();
-        ArrayList<Integer> claves=new ArrayList<>();
-        while (rs.next()){
+        ResultSet rs = stt.getGeneratedKeys();
+        ArrayList<Integer> claves = new ArrayList<>();
+        while (rs.next()) {
             claves.add(rs.getInt(1));
         }
         return claves;
@@ -128,7 +129,7 @@ public abstract class Conexion {
      * metodo para cargar datos dentro de un prepared statement multifilas
      * usando una matriz de clase object y un prepared statement para poder cargar los datos en el Prepared statement
      *
-     * @param stt stt
+     * @param stt        stt
      * @param parametros parametros
      * @throws Exception Throws Exception
      */
@@ -141,14 +142,14 @@ public abstract class Conexion {
                 stt.setInt(j, (Integer) parametros[i]);
             } else if (parametros[i] instanceof Double) {
                 stt.setDouble(j, (Double) parametros[i]);
-            }else if(parametros[i] instanceof java.util.Date){
-                java.util.Date date=(java.util.Date)parametros[i];
+            } else if (parametros[i] instanceof java.util.Date) {
+                java.util.Date date = (java.util.Date) parametros[i];
                 LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
                 int month = localDate.getMonthValue();
                 int day = localDate.getDayOfMonth();
                 int year = localDate.getYear();
-                java.sql.Date fechabd=new java.sql.Date(year,month,day);
-                stt.setDate(j,fechabd);
+                java.sql.Date fechabd = new java.sql.Date(year, month, day);
+                stt.setDate(j, fechabd);
             }
         }
     }
@@ -158,13 +159,13 @@ public abstract class Conexion {
      * metodo para cargar datos dentro de un prepared statement
      * usando un array de clase object y un prepared statement para poder cargar los datos en el Prepared statement
      *
-     * @param stt stt
+     * @param stt        stt
      * @param parametros parametros
      * @throws Exception Throws Exception
      */
     private static void cargarMultiDatos(PreparedStatement stt, Object[][] parametros) throws Exception {
-        int n=1;
-        for (int k = 0; k <parametros.length; k++) {
+        int n = 1;
+        for (int k = 0; k < parametros.length; k++) {
 
             for (int i = 0; i < parametros[k].length; i++) {
 
@@ -184,12 +185,12 @@ public abstract class Conexion {
                     int year = localDate.getYear();
                     java.sql.Date fechabd = new java.sql.Date(date.getTime());
                     stt.setDate(n, fechabd);
-  //                  System.out.println(fechabd);
+                    //                  System.out.println(fechabd);
                 }
                 n++;
             }
         }
-    //    System.out.println(n);
+        //    System.out.println(n);
     }
 
 }
